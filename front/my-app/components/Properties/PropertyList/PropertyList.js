9a85791b6718cb25129f21f06eeee88b91862171
Fuzehu <foti.n2a@gmail.com>
2023-07-23 04:04:31 +0200

    last front update properties list

--- front/my-app/components/Properties/PropertyList/PropertyList.js
+++ front/my-app/components/Properties/PropertyList/PropertyList.js
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react'
-import { Box, Button, Flex, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Text, UnorderedList, ListItem } from '@chakra-ui/react'
 import Link from 'next/link'
 
 const PropertyList = () => {
@@ -9,8 +9,7 @@ const PropertyList = () => {
             <Box w="500px" m="2">
                 <Box h="500px" p="2" bg="lightblue" borderRadius="md" boxShadow="lg" borderColor="darkslateblue" borderWidth="2px"
                     backgroundImage="/images/Morgon.png" backgroundSize="cover">
-                    <Flex direction="column" h="100%" justifyContent="space-between" alignItems="center" p={5}>
-                        <Text fontSize="2xl" color="black">MVS - Morgon</Text>
+                    <Flex direction="column" h="100%" justifyContent="flex-end" alignItems="center" p={5}>
                         <Link href="/ourListings/morgon">
                             <Button colorScheme="purple">
                               Access Presale
@@ -18,16 +17,23 @@ const PropertyList = () => {
                         </Link>
                     </Flex>
                 </Box>
-                <Box bg="lavender" p="2" borderRadius="md" boxShadow="lg" mt="2" borderColor="darkslateblue" borderWidth="2px">
-                    <Text>Property Informations</Text>
+                <Box bg="lavender" p="4" borderRadius="md" boxShadow="lg" mt="2" borderColor="darkslateblue" borderWidth="2px">
+                    <Text fontWeight="bold" mb="2">Property Informations:</Text>
+                    <Box bg="white" p="2" borderRadius="md" boxShadow="lg" mt="2" borderColor="darkslateblue" borderWidth="2px" mb="2">
+                        <Text fontWeight="bold">Location and Size:</Text>
+                        <Text>Located in AOP Morgon, this property sprawls across 1 hectare.</Text>
+                    </Box>
+                    <Box bg="white" p="2" borderRadius="md" boxShadow="lg" mt="2" borderColor="darkslateblue" borderWidth="2px">
+                        <Text fontWeight="bold">Parcel Value:</Text>
+                        <Text>The parcel is valued at $100,000.</Text>
+                    </Box>
                 </Box>
             </Box>
     
             <Box w="500px" m="2">
                 <Box h="500px" p="2" bg="lightblue" borderRadius="md" boxShadow="lg" borderColor="darkslateblue" borderWidth="2px"
                     backgroundImage="url('https://images.winalist.com/blog/wp-content/uploads/2021/03/26144459/AdobeStock_211595652-1-copy.jpeg')" backgroundSize="cover">
-                    <Flex direction="column" h="100%" justifyContent="space-between" alignItems="center" p={5}>
-                        <Text fontSize="2xl" color="black">MVS - StEmilion</Text>
+                    <Flex direction="column" h="100%" justifyContent="flex-end" alignItems="center" p={5}>
                         <Link href="/ourListings/stEmilion">
                             <Button colorScheme="purple">
                               Access Presale
@@ -35,8 +41,19 @@ const PropertyList = () => {
                         </Link>
                     </Flex>
                 </Box>
-                <Box bg="lavender" p="2" borderRadius="md" boxShadow="lg" mt="2" borderColor="darkslateblue" borderWidth="2px">
-                    <Text>Property Informations</Text>
+                <Box bg="lavender" p="4" borderRadius="md" boxShadow="lg" mt="2" borderColor="darkslateblue" borderWidth="2px">
+                    <Text fontWeight="bold" mb="2">Property Informations:</Text>
+                    <Box bg="white" p="2" borderRadius="md" boxShadow="lg" mt="2" borderColor="darkslateblue" borderWidth="2px" mb="2">
+                        <Text fontWeight="bold">Location and Size:</Text>
+                        <Text>Located in AOP St-Emilion, this property sprawls across 3.5 hectares.</Text>
+                    </Box>
+                    <Box bg="white" p="2" borderRadius="md" boxShadow="lg" mt="2" borderColor="darkslateblue" borderWidth="2px" mb="2">
+                        <Text fontWeight="bold">Parcel Value:</Text>
+                        <Text>The parcel is valued at $1.2 million.</Text>
+                    </Box>
+                    <Box bg="lightgray" p="2" borderRadius="md" boxShadow="lg" mt="2" borderColor="darkslateblue" borderWidth="2px">
+                        <Text fontWeight="bold">Coming Soon!</Text>
+                    </Box>
                 </Box>
             </Box>
         </Flex>
